6f93b32b6befa2788388e7239ec205b41b9472f6
Kalpesh Khairnar <71657135+khairnarkalpesh@users.noreply.github.com>
2022-08-30 23:37:51 +0530

    login/logout added

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
-
 const errorMiddleware = require("./middleware/error");
-
-app.use(express.json())
+const cookieParser = require("cookie-parser");
 
 // Routes import
-const product = require("./routes/productRoute")
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
 
+app.use(express.json())
+app.use(cookieParser());
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 // Middleware for errors
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+// Register a User
+exports.registerUser = catchAsyncErrors( async (req, res, next) => {
+    const {name, email, password} = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id:"This is sample",
+            url:"profilepic"
+        },
+    });
+
+    const token = user.getJWTToken();
+
+    sendToken(user, 201, res);
+})
+
+// Login user
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    // Checking if user have given email and password
+    if(!email || !password){
+        return next(new ErrorHandler("Please enter email and password", 400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user){
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const token = user.getJWTToken();
+
+    sendToken(user, 200, res);
+
+});
+
+// Logout user
+exports.logout = catchAsyncErrors( async (req, res, next) => {
+
+    res.cookie("token", null, {
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success:true,
+        message:"Logged out"
+    })
+})
